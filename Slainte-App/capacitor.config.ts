4686828dcc545f92bc444b65d48a69235e1a6d92
katylynch-1 <Katylynch0501@gmail.com>
2024-07-25 12:35:43 +0100

    Install capacitor and ran build for ios on simulator

--- Slainte-App/capacitor.config.ts
+++ Slainte-App/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Slainte-App',
+  appId: 'com.example.app',
+  appName: 'slainte-app',
   webDir: 'www'
 };
 
