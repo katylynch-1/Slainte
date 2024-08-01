7978b0c042140b90a492ec3e8f6de71f0488c59a
katylynch-1 <Katylynch0501@gmail.com>
2024-08-01 13:34:30 +0100

    Got user location working so that it displays it on a map
    Installed capacitor geolocation and used Maps Javascript API

--- Slainte-App/src/app/tab1/tab1.page.ts
+++ Slainte-App/src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from '../services/geolocation.service';
+
+declare var google: any;
 
 @Component({
   selector: 'app-tab1',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab1Page implements OnInit {
 
-  constructor() {}
+  map: any;
+
+  constructor(private geolocationService: GeolocationService) {}
 
   ngOnInit() {
+    this.loadMap();
+  }
+
+  async loadMap() {
+    const { lat, lng } = await this.geolocationService.getCurrentPosition();
+
+    const mapOptions = {
+      center: { lat: lat, lng: lng },
+      zoom: 15
+    };
+
+    const mapEle = document.getElementById('map');
+    this.map = new google.maps.Map(mapEle, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: { lat: lat, lng: lng },
+      map: this.map
+    });
   }
 
   
