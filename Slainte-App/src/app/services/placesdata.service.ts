b3a5fdf90e6ca6fbb583931aa697b1fe49716ad1
katylynch-1 <Katylynch0501@gmail.com>
2024-09-24 16:21:36 +0100

    Deleted duplicate cors

--- Slainte-App/src/app/services/placesdata.service.ts
+++ Slainte-App/src/app/services/placesdata.service.ts
@@ -7,12 +7,11 @@ import { Observable } from 'rxjs';
 })
 export class PlacesdataService {
 
-  private serverUrl = 'http://localhost:3000/api/place'; // Ensure this is the correct endpoint
+  private serverUrl = 'http://localhost:3000/api/places'; // I think this has to match line 12 on backend server
 
   constructor(private http: HttpClient) {}
 
   getBars(lat: number, lng: number, radius: number): Observable<any> {
-    // No need to pass type or API key. The server handles it.
     return this.http.get(`${this.serverUrl}?lat=${lat}&lng=${lng}&radius=${radius}`);
   }
 }
